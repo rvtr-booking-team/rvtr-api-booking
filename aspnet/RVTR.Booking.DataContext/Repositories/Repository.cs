f0c8f621f52a9f8a1dd74cc20e220ec63e653235
fred belotte <fredbelotte@outlook.com>
2020-04-16 00:57:22 -0500

    add booking repository scaffolding [#172281007]

---
+++ aspnet/RVTR.Booking.DataContext/Repositories/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RVTR.Booking.DataContext.Repositories
+{
+  public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+  {
+    public bool Delete(int id) => throw new System.NotImplementedException();
+
+    public bool Insert(TEntity entity) => throw new System.NotImplementedException();
+
+    public IEnumerable<TEntity> Select() => throw new System.NotImplementedException();
+
+    public TEntity Select(int id) => throw new System.NotImplementedException();
+
+    public bool Update(TEntity entity) => throw new System.NotImplementedException();
+  }
+}
---
+++ aspnet/RVTR.Booking.DataContext/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace RVTR.Booking.DataContext.Repositories
+{
+  public class UnitOfWork
+  {
+    public void Commit() => throw new NotImplementedException();
+  }
+}

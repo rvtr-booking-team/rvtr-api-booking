4c6bd0c0653be8eb0e88b9870c787b1bedb5b88b
Roby Thomas <roby.thomas1020@gmail.com>
2020-04-21 20:22:45 -0500

    fix formatting in reservation [#172282248]

--- aspnet/RVTR.Booking.ObjectModel/Models/Reservation.cs
+++ aspnet/RVTR.Booking.ObjectModel/Models/Reservation.cs
@@ -3,22 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RVTR.Booking.ObjectModel.Models
 {
-    public class Reservation : IValidatableObject
-    {
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => throw new System.NotImplementedException();
+  public class Reservation : IValidatableObject
+  {
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => throw new System.NotImplementedException();
 
-        [Required]
-        public int ReservationId { get; set; }
-        [Required]
-        public int AccountId { get; set; }
-        [Required]
-        public int RentalId { get; set; }
-        [Required]
-        public Duration Duration { get; set; }
-        [Required]
-        public Status Status { get; set; }
-        [Required]
-        public List<Guest> Guests { get; set; }
-        public string Notes { get; set; }
-    }
+    [Required]
+    public int ReservationId { get; set; }
+    [Required]
+    public int AccountId { get; set; }
+    [Required]
+    public int RentalId { get; set; }
+    [Required]
+    public Duration Duration { get; set; }
+    [Required]
+    public Status Status { get; set; }
+    [Required]
+    public List<Guest> Guests { get; set; }
+    public string Notes { get; set; }
+  }
 }

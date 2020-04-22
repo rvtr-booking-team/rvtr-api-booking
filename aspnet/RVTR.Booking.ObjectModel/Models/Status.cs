bc60ba6df83e275247be19de535045484cee43d7
Roby Thomas <roby.thomas1020@gmail.com>
2020-04-22 13:23:52 -0500

    test status test [#172282248]

--- aspnet/RVTR.Booking.ObjectModel/Models/Status.cs
+++ aspnet/RVTR.Booking.ObjectModel/Models/Status.cs
@@ -16,7 +16,8 @@ namespace RVTR.Booking.ObjectModel.Models
         || StatusName.ToLower() != "pending"
         || StatusName.ToLower() != "canceled")
       {
-        yield return new ValidationResult("Status Name must be 'confirmed', 'pending' or 'canceled'");
+        //yield return new ValidationResult("Status Name must be 'confirmed', 'pending' or 'canceled'");
+        yield return new ValidationResult("test status test");
       }
     }
   }
---
+++ aspnet/RVTR.Booking.UnitTesting/Tests/Status_Test.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using RVTR.Booking.DataContext.Repositories;
+using Xunit;
+using RVTR.Booking.ObjectModel.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace RVTR.Booking.UnitTesting.Tests
+{
+  public class Status_Test
+  {
+    [Fact]
+    public void Test_StatusValidation()
+    {
+      Status status = new Status
+      {
+        StatusId = 1,
+        StatusName = null
+      };
+      Assert.True(ValidateModel(status).Any(
+          s => s.MemberNames.Contains("StatusName") &&
+               s.ErrorMessage.Contains("Status is required")));
+    }
+
+    private IList<ValidationResult> ValidateModel(object model)
+    {
+      var validationResults = new List<ValidationResult>();
+      var ctx = new ValidationContext(model, null, null);
+      Validator.TryValidateObject(model, ctx, validationResults, true);
+      return validationResults;
+    }
+  }
+}

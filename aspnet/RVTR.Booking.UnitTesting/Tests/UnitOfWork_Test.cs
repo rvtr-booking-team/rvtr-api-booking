6b2a138896951c767845ebf407d42d128b963ed2
tew9 <tango.tew@outlook.com>
2020-04-23 21:30:13 -0500

    add test for searchAll, searchone [#172469619]

--- aspnet/RVTR.Booking.UnitTesting/Tests/UnitOfWork_Test.cs
+++ aspnet/RVTR.Booking.UnitTesting/Tests/UnitOfWork_Test.cs
@@ -44,25 +44,80 @@ namespace RVTR.Booking.UnitTesting.Tests
     [Fact]
     public void Add_status_to_database()
     {
-        var TestEntity = new Status(){StatusId = 1, StatusName = "pending"};
-        var options = new DbContextOptionsBuilder<BookingDbContext>()
-            .UseInMemoryDatabase(databaseName: "Add_status_to_database")
-            .Options;
+      var TestEntity = new Status(){StatusId = 1, StatusName = "pending"};
+      var options = new DbContextOptionsBuilder<BookingDbContext>()
+          .UseInMemoryDatabase(databaseName: "Add_status_to_database")
+          .Options;
 
-        // Run the test against one instance of the context
-        var context = new BookingDbContext(options);
-        var unWork = new UnitOfWork(context);
-        var StatusRepo = unWork.StatusRepository;
-        StatusRepo.Insert(TestEntity);
-        unWork.Commit();
+      // Run the test against one instance of the context
+      var context = new BookingDbContext(options);
+      var unWork = new UnitOfWork(context);
+      var StatusRepo = unWork.StatusRepository;
+      StatusRepo.Insert(TestEntity);
+      unWork.Commit();
 
-        // Use a separate instance of the context to verify correct data was saved to database
-        using(var context2 = new BookingDbContext(options))
-        {
-          Assert.Equal(1, context2.Status.Count());
-          Assert.Equal(TestEntity, context.Status.Single());
-        }
+      // Use a separate instance of the context to verify correct data was saved to database
+      using(var context2 = new BookingDbContext(options))
+      {
+        Assert.Equal(1, context2.Status.Count());
+        Assert.Equal(TestEntity, context.Status.Single());
       }
     }
- }
+
+    [Fact]
+    public void SelectAll_status_from_database()
+    {
+      var options = new DbContextOptionsBuilder<BookingDbContext>()
+          .UseInMemoryDatabase(databaseName: "selectAll_database")
+          .Options;
+
+      // Run the test against one instance of the context
+      var context = new BookingDbContext(options);
+      var unWork = new UnitOfWork(context);
+      var StatusRepo = unWork.StatusRepository;
+
+      //Seed some data to status to query from.
+      context.Status.Add(new Status(){StatusId = 2, StatusName = "Complete"});
+      context.Status.Add(new Status(){StatusId = 3, StatusName = "Complete"});
+      context.Status.Add(new Status(){StatusId = 6, StatusName = "Canceled"});
+      unWork.Commit();
+
+      // Use a separate instance of the context to verify correct data was saved to database
+      using(var context2 = new BookingDbContext(options))
+      {
+        var unitWork = new UnitOfWork(context);
+        var result = unitWork.StatusRepository.Select();
+        Assert.Equal(3, result.Count());
+      }
+    }
+
+    [Fact]
+    public void SelectOne_status_from_database()
+    {
+      var TestEntity = new Status(){StatusId = 3, StatusName = "Complete"};
+      var options = new DbContextOptionsBuilder<BookingDbContext>()
+          .UseInMemoryDatabase(databaseName: "selectOne_database")
+          .Options;
+
+      // Run the test against one instance of the context
+      var context = new BookingDbContext(options);
+      var unWork = new UnitOfWork(context);
+      var StatusRepo = unWork.StatusRepository;
+
+      //Seed some data to status to query from.
+      context.Status.Add(new Status(){StatusId = 2, StatusName = "Complete"});
+      context.Status.Add(new Status(){StatusId = 4, StatusName = "Canceled"});
+      context.Status.Add(TestEntity);
+      unWork.Commit();
+
+      // Use a separate instance of the context to verify correct data was saved to database
+      using(var context2 = new BookingDbContext(options))
+      {
+        var unitWork = new UnitOfWork(context);
+        var result = unitWork.StatusRepository.Select(3);
+        Assert.Equal(TestEntity, result);
+      }
+    }
+  }
+}
 

0d8fd4c3b4d9e054847336b076f4107ebe878b55
gjjuarez <george.jose.juarez@gmail.com>
2020-04-27 10:17:17 -0500

    Add documentation for Status and Guest controllers [#172506018]

--- aspnet/RVTR.Booking.WebApi/Controllers/GuestController.cs
+++ aspnet/RVTR.Booking.WebApi/Controllers/GuestController.cs
@@ -9,6 +9,10 @@ using RVTR.Booking.ObjectModel.Models;
 
 namespace RVTR.Booking.WebApi.Controllers
 {
+  /// <summary>
+  /// Guest controller that allows CRUD operations on
+  /// the Guest Repository.
+  /// </summary>
   [ApiController]
   [EnableCors()]
   [Route("[controller]/[action]")]
@@ -17,24 +21,44 @@ namespace RVTR.Booking.WebApi.Controllers
     private readonly ILogger<GuestController> _logger;
     private readonly IUnitOfWork _unitOfWork;
 
+    /// <summary>
+    /// Dependency Injection
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="unitOfWork"></param>
     public GuestController(ILogger<GuestController> logger, IUnitOfWork unitOfWork)
     {
       _logger = logger;
       _unitOfWork = unitOfWork;
     }
 
+    /// <summary>
+    /// Returns every Guest from the repository.
+    /// </summary>
+    /// <returns>List of Guest</returns>
     [HttpGet]
     public async Task<IEnumerable<Guest>> Get()
     {
       return await Task.FromResult<IEnumerable<Guest>>(_unitOfWork.GuestRepository.Select());
     }
 
+    /// <summary>
+    /// Returns a Guest that matches the ID
+    /// that was given.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Guest</returns>
     [HttpGet("{id}")]
     public async Task<Guest> GetOne(int id)
     {
       return await Task.FromResult<Guest>(_unitOfWork.GuestRepository.Select(id));
     }
 
+    /// <summary>
+    /// Will add a Guest to the repository.
+    /// </summary>
+    /// <param name="guest"></param>
+    /// <returns>Request success or failure</returns>
     [HttpPost]
     public async Task<IActionResult> Post(Guest guest)
     {
@@ -46,6 +70,12 @@ namespace RVTR.Booking.WebApi.Controllers
       return BadRequest();
     }
 
+    /// <summary>
+    /// Will add a Guest to the repository or updating
+    /// it if it already exists.
+    /// </summary>
+    /// <param name="guest"></param>
+    /// <returns>Request success or failure</returns>
     [HttpPut]
     public async Task<IActionResult> Put(Guest guest)
     {
@@ -57,6 +87,12 @@ namespace RVTR.Booking.WebApi.Controllers
       return BadRequest();
     }
 
+    /// <summary>
+    /// Deletes a Guest from the repository that
+    /// matches the given ID.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Request success or failure</returns>
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
--- aspnet/RVTR.Booking.WebApi/Controllers/StatusController.cs
+++ aspnet/RVTR.Booking.WebApi/Controllers/StatusController.cs
@@ -9,6 +9,10 @@ using RVTR.Booking.ObjectModel.Models;
 
 namespace RVTR.Booking.WebApi.Controllers
 {
+  /// <summary>
+  /// Status controller that allows CRUD operations on
+  /// the Status repository
+  /// </summary>
   [ApiController]
   [EnableCors()]
   [Route("[controller]/[action]")]
@@ -17,24 +21,43 @@ namespace RVTR.Booking.WebApi.Controllers
     private readonly ILogger<StatusController> _logger;
     private readonly IUnitOfWork _unitOfWork;
 
+    /// <summary>
+    /// Dependency Injection
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="unitOfWork"></param>
     public StatusController(ILogger<StatusController> logger, IUnitOfWork unitOfWork)
     {
       _logger = logger;
       _unitOfWork = unitOfWork;
     }
 
+    /// <summary>
+    /// Returns every Status.
+    /// </summary>
+    /// <returns>List of Status</returns>
     [HttpGet]
     public async Task<IEnumerable<Status>> Get()
     {
       return await Task.FromResult<IEnumerable<Status>>(_unitOfWork.StatusRepository.Select());
     }
 
+    /// <summary>
+    /// Returns a Status that matches the id given.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Status<returns>
     [HttpGet("{id}")]
     public async Task<Status> GetOne(int id)
     {
       return await Task.FromResult<Status>(_unitOfWork.StatusRepository.Select(id));
     }
 
+    /// <summary>
+    /// Will add a Status to the repository.
+    /// </summary>
+    /// <param name="status"></param>
+    /// <returns>Request success or failure</returns>
     [HttpPost]
     public async Task<IActionResult> Post(Status status)
     {
@@ -46,6 +69,12 @@ namespace RVTR.Booking.WebApi.Controllers
       return BadRequest();
     }
 
+    /// <summary>
+    /// Will add a Status to the repository or updating
+    /// it if it already exists.
+    /// </summary>
+    /// <param name="status"></param>
+    /// <returns>Request success or failure</returns>
     [HttpPut]
     public async Task<IActionResult> Put(Status status)
     {
@@ -57,6 +86,12 @@ namespace RVTR.Booking.WebApi.Controllers
       return BadRequest();
     }
 
+    /// <summary>
+    /// Deletes a Status from the repository that
+    /// matches the given ID.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Request success or failure</returns>
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
